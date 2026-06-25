845ac08dc2edda68ac0e0f0611fbf8d060af0696
HiAbdounour <abdounour.misbah@universite-paris-saclay.fr>
2026-06-25 19:27:42 +0200

    First layout modif

--- app/index.tsx
+++ app/index.tsx
@@ -9,6 +9,8 @@ export default function Index() {
         Ceci est un test.
       </Text>
       <Link href='/onboarding' className='mt-4 rounded bg-primary text-white p-4'>Go to onboarding page</Link>
+      <Link href='/(auth)/signin' className='mt-4 rounded bg-primary text-white p-4'>Sign In</Link>
+      <Link href='/(auth)/signup' className='mt-4 rounded bg-primary text-white p-4'>Sign Up</Link>
     </View>
   );
 }
