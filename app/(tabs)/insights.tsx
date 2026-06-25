ce369081b95eb47e6c0e286977c171a753140556
HiAbdounour <abdounour.misbah@universite-paris-saclay.fr>
2026-06-25 21:01:28 +0200

    Fix some bugs

--- app/(tabs)/insights.tsx
+++ app/(tabs)/insights.tsx
@@ -1,5 +1,4 @@
-import { View,Text } from 'react-native'
-import { Link } from 'expo-router'
+import { Text } from 'react-native'
 import { styled } from "nativewind"
 import { SafeAreaView as RawSafeAreaView } from 'react-native-safe-area-context';
 
