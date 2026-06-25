b9bd0b090a29a24cdf9b881a2540fb600b0dd18d
HiAbdounour <abdounour.misbah@universite-paris-saclay.fr>
2026-06-25 20:36:02 +0200

    Create the main theme

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,12 +1,47 @@
+import { tabs } from "@/constants/data";
 import { Tabs } from "expo-router";
+import { View,Image } from "react-native";
+import clsx from "clsx";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 
-const TabLayout = () => (
-    <Tabs screenOptions={{headerShown:false}}>
-        <Tabs.Screen name='index' options={{title:"Home"}}/>
-        <Tabs.Screen name='subscriptions' options={{title:"Subscriptions"}}/>
-        <Tabs.Screen name='insights' options={{title:"Insights"}}/>
-        <Tabs.Screen name='settings' options={{title:"Settings"}}/>
-    </Tabs>
-)
+const TabLayout = () => {
+    const insets = useSafeAreaInsets();
+    const TabIcon = ({focused,icon}:TabIconProps) => {
+        return(
+            <View className="tabs-icon">
+                <View className={clsx("tabs-pill",focused&&"tabs-active")}>
+                    <Image 
+                        source={icon}
+                        className="tabs-glyph"
+                        resizeMode='contain'
+                    />
+                </View>
+            </View>
+    )};
+
+    return(
+        <Tabs screenOptions={{
+            headerShown:false,
+            tabBarShowLabel:false,
+            tabBarStyle:{
+                position:"absolute",
+                Math.max(insets.bottom,)
+            }
+        }}>
+            {tabs.map((tab)=>(
+                <Tabs.Screen
+                    key={tab.name}
+                    name={tab.name}
+                    options={{
+                        title: tab.title,
+                        tabBarIcon: ({focused})=>(<TabIcon focused={focused} icon={tab.icon}/>)
+                    }}
+                />
+            ))}
+        </Tabs>
+    )
+}
 
 export default TabLayout
+
+// <Tabs.Screen name='subscriptions/[id]' options={{href:null}}/>
--- constants/data.ts
+++ constants/data.ts
@@ -1,6 +1,6 @@
 import { icons } from "./icon";
 
-export const iconsTab = [
+export const tabs = [
     {name:'index',title:'Home',icon:icons.home},
     {name:'subscriptions',title:'Subscriptions',icon:icons.wallet},
     {name:'insights',title:'Insights',icon:icons.activity},
