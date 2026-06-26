c796cf2a9bbbeabb0f93d21998c6a7e9470fa9e4
HiAbdounour <abdounour.misbah@universite-paris-saclay.fr>
2026-06-26 17:31:29 +0200

    Use SubscriptionCard into index.tsx

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,16 +4,20 @@ import { Link } from 'expo-router';
 import { Text,View,Image, FlatList } from "react-native";
 import { SafeAreaView as RawSafeAreaView } from 'react-native-safe-area-context';
 import image from '@/constants/image';
-import { HOME_BALANCE, HOME_USER, UPCOMING_SUBSCRIPTIONS } from '@/constants/data';
+import { HOME_BALANCE, HOME_SUBSCRIPTIONS, HOME_USER, UPCOMING_SUBSCRIPTIONS } from '@/constants/data';
 import { icons } from '@/constants/icon';
 import { formatCurrency } from '@/lib/USD';
 import dayjs from 'dayjs';
 import ListHeading from '../components/listHeading';
 import UpcomingCard from '../components/upcomingCard';
+import { useState } from 'react';
+import SubscriptionCard from '../components/subscriptionCard';
 
 const SafeAreaView = styled(RawSafeAreaView);
 
 export default function App() {
+  const [expandedId,setExpandedId] = useState<string|null>(null);
+
   return (
     <SafeAreaView className='flex-1 bg-background p-5'>
       <View className="home-header">
@@ -53,6 +57,13 @@ export default function App() {
 
       <View>
         <ListHeading title='All Subscriptions'/>
+        <SubscriptionCard
+          {...HOME_SUBSCRIPTIONS[0]}
+          expanded={expandedId===HOME_SUBSCRIPTIONS[0].id}
+          onPress={()=>setExpandedId((currentId)=>(
+            currentId===HOME_SUBSCRIPTIONS[0].id ? null : HOME_SUBSCRIPTIONS[0].id
+          ))}
+        />
       </View>
     </SafeAreaView>
   );
