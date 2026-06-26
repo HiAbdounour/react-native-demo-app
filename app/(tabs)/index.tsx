defcebadb854452533f1d9d90ecc2c30c17a6231
HiAbdounour <abdounour.misbah@universite-paris-saclay.fr>
2026-06-26 16:31:10 +0200

    Use font example

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,8 +9,8 @@ const SafeAreaView = styled(RawSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className='flex-1 bg-background p-5'>
-      <Text className='text-xl font-bold text-success'>
-        Ceci est un test.
+      <Text className='text-7xl font-sans-bold text-success'>
+        This is a demo.
       </Text>
       <Link href='/onboarding' className='mt-4 rounded bg-primary text-white p-4'>Go to onboarding page</Link>
       <Link href='/(auth)/signin' className='mt-4 rounded bg-primary text-white p-4'>Sign In</Link>
