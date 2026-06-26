7d44e60c83d56b7bf860c77063e1a72765f0b6e1
HiAbdounour <abdounour.misbah@universite-paris-saclay.fr>
2026-06-26 16:35:22 +0200

    Add subdata

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -9,12 +9,12 @@ const SafeAreaView = styled(RawSafeAreaView);
 export default function App() {
   return (
     <SafeAreaView className='flex-1 bg-background p-5'>
-      <Text className='text-7xl font-sans-bold text-success'>
+      <Text className='text-5xl font-sans-bold text-success'>
         This is a demo.
       </Text>
-      <Link href='/onboarding' className='mt-4 rounded bg-primary text-white p-4'>Go to onboarding page</Link>
-      <Link href='/(auth)/signin' className='mt-4 rounded bg-primary text-white p-4'>Sign In</Link>
-      <Link href='/(auth)/signup' className='mt-4 rounded bg-primary text-white p-4'>Sign Up</Link>
+      <Link href='/onboarding' className='mt-4 font-sans-semibold rounded bg-primary text-white p-4'>Go to onboarding page</Link>
+      <Link href='/(auth)/signin' className='mt-4 font-sans-semibold rounded bg-primary text-white p-4'>Sign In</Link>
+      <Link href='/(auth)/signup' className='mt-4 font-sans-semibold rounded bg-primary text-white p-4'>Sign Up</Link>
     
     
       <Link href={{
